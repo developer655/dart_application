0900268b7cf3116544abe3a2b712ea270a1e5634
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2023-04-03 10:14:15 +0600

    collection_filter

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,14 @@
-import 'model/data.dart';
+import 'model/entity.dart';
+import 'utils/collection_helper.dart';
 
 void main(List<String> arguments) {
-  final collection = List<Data>.generate(100, (index) => Data("$index"));
+  final collection = List<Entity>.generate(100, (index) => Entity("$index"));
   final ids = ["6", "50", "30", "35"];
-  final specific =
-      collection.where((element) => ids.contains(element.id)).toList();
+  final specific = CollectionHelper.filter(
+    collection: collection,
+    filter: (item) {
+      return ids.contains(item.id);
+    },
+  );
   print(specific);
 }
